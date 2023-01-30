6240f543a576e96a0d8f23315ced08f86cbd8fce
Odilbobanazarov <116312437+Odilbobanazarov@users.noreply.github.com>
2023-01-30 13:38:09 -0600

    Update security_group.tf

--- rds/ticket1/security_group.tf
+++ rds/ticket1/security_group.tf
@@ -1,9 +1,18 @@
 resource "aws_security_group" "db" {
-  name_prefix        = "db"
-  # Your code goes here 
-  
-  
-  
-  # Please do not change the code below
-  tags = local.task_tags
-}
+  name_prefix = "db"
+  description = "Allow TLS inbound traffic"
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
