ec7c24f274641512a342e3e6a64f1f2f29c51f86
Odilbobanazarov <116312437+Odilbobanazarov@users.noreply.github.com>
2023-01-30 13:37:32 -0600

    Update rds.tf

--- rds/ticket1/rds.tf
+++ rds/ticket1/rds.tf
@@ -1,8 +1,18 @@
 resource "aws_db_instance" "default" {
-  # Your code goes here 
-  
-  
-  
+  allocated_storage          = 50
+  max_allocated_storage      = 100
+  db_name                    = "mydb"
+  engine                     = "mysql"
+  engine_version             = "5.7"
+  instance_class             = "db.t3.micro"
+  username                   = "foo"
+  password                   = "foobarbaz"
+  parameter_group_name       = "default.mysql5.7"
+  skip_final_snapshot        = true
+  auto_minor_version_upgrade = true
+
+
+
   # Please do not change below code
   tags = local.task_tags
 }
