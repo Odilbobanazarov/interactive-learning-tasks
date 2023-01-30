5674cbb4883e4b006b6469826dc915d81e0e3e4f
Odilbobanazarov <116312437+Odilbobanazarov@users.noreply.github.com>
2023-01-30 13:46:00 -0600

    Update s3.tf

--- s3/ticket1/s3.tf
+++ s3/ticket1/s3.tf
@@ -1,11 +1,13 @@
 resource "aws_s3_bucket" "ticket1" {
-  bucket_prefix = 
-  # Your code goes here 
-  
-  
-  
-  
-  
-  # Please do not change below code
-  tags = local.task_tags
+ bucket_prefix = "aws-s3-devops-task-Odil"
+ acl = "private"
+ }
+ resource "aws_s3_bucket_versioning" "ticket1" {
+   bucket = aws_s3_bucket.ticket1.id
+   versioning_configuration {
+     status = "Enabled"
+   }
+ # Please do not change below code
+ tags = local.task_tags
 }
+
