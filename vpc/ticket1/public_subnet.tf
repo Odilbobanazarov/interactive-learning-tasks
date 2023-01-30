69cffbfd022d6fa4744ae526cb9124281339c40a
Odilbobanazarov <116312437+Odilbobanazarov@users.noreply.github.com>
2023-01-30 13:51:55 -0600

    Update public_subnet.tf

--- vpc/ticket1/public_subnet.tf
+++ vpc/ticket1/public_subnet.tf
@@ -1,5 +1,9 @@
 resource "aws_subnet" "public1" {
-  # Your code goes here 
+vpc_id                 = aws_vpc.main.id
+cidr_block             = "10.0.1.0/24"
+availability_zone      = "eu-west-1a"
+map_public_ip_on_launch = true
+
 
 
 
@@ -10,10 +14,11 @@ resource "aws_subnet" "public1" {
   tags = local.task_tags
 }
 
-
 resource "aws_subnet" "public2" {
-   # Your code goes here 
-
+vpc_id                 = aws_vpc.main.id
+cidr_block             = "10.0.2.0/24"
+availability_zone      = "eu-west-1b"
+map_public_ip_on_launch = true
 
 
 
@@ -26,7 +31,10 @@ resource "aws_subnet" "public2" {
 
 
 resource "aws_subnet" "public3" {
-   # Your code goes here 
+vpc_id                 = aws_vpc.main.id
+cidr_block             = "10.0.3.0/24"
+availability_zone      = "eu-west-1c"
+map_public_ip_on_launch = true
 
 
 
