3e1f911b10e013f401045cfca6926ced0432ccd5
Odilbobanazarov <116312437+Odilbobanazarov@users.noreply.github.com>
2023-01-30 13:49:49 -0600

    Update output.tf

--- vpc/ticket1/output.tf
+++ vpc/ticket1/output.tf
@@ -1,7 +1,17 @@
-output "id" {
-  # Your code goes here
+output address {
+  value = "aws_db_instance.default.address"
+}
+output availability_zone {
+  value = "aws_db_instance.default.availability_zone"
+}
+output engine {
+  value = "aws_db_instance.default.engine"
+}
+output id {
+  value = "aws_db_instance.default.id"
+}
+output port {
+  value = "aws_db_instance.default.port"
 }
 
-output "arn" {
-   # Your code goes here
-}
+
