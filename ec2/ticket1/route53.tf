280e66891b41e3db075e53cf450072a14664d531
Odilbobanazarov <116312437+Odilbobanazarov@users.noreply.github.com>
2023-01-30 13:31:17 -0600

    Update route53.tf

--- ec2/ticket1/route53.tf
+++ ec2/ticket1/route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "wordpress" {
-  zone_id = # Your code goes here
-  name    = # Your code goes here
+  zone_id = "Z0979855134EVEQC3D0PS"
+  name    = "wordpress.odilcyberdevop.com"
   type    = "A"
   ttl     = "300"
-  records = [# Your code goes here]
+  records = [aws_instance.web.public_ip]
 }
